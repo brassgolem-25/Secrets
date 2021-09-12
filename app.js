508213e6858d7cc6557ef4bd1158fd2f38090a9e
Tushar <tushartiwari2002@gamil.com>
2021-09-12 15:55:40 +0530

    env and L-3

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-var encrypt = require('mongoose-encryption');
+// var encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -22,7 +23,7 @@ const userSchema = new mongoose.Schema({
 })
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['password'] });
 
 const User =  mongoose.model("User",userSchema);
 app.get('/',(req,res)=>{
@@ -40,7 +41,7 @@ app.get('/register',(req,res)=>{
 app.post('/register',(req,res)=>{
   const newUser = new User({
     email : req.body.username,
-    password : req.body.password
+    password : md5(req.body.password)
   })
   newUser.save((err) => {
     if(err){
@@ -55,7 +56,7 @@ app.post('/login',(req,res)=>{
   User.findOne({email: req.body.username},(err,foundUser) => {
     if(!err){
       if(foundUser){
-        if(foundUser.password === req.body.password){
+        if(foundUser.password === md5(req.body.password)){
           res.render('secrets');
         }
       }
